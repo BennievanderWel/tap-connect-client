e86b049e3ea2ecfeed52b555c0ba07229d6cc5c6
Bennie van der Wel <bennievanderwel@gmail.com>
2018-08-18 08:49:30 +0200

    first commit. Added prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'react/prefer-stateless-function': 'off',
+    semi: [2, 'never'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+  }
+}
